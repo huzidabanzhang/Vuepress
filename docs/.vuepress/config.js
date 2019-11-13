5df3352e3292a14b482b2abb4ef3435eb730535e
大胡子 <375532103@qq.com>
2019-11-13 15:31:36 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '章胖胖的笔记',
+    description: 'Hello',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'GitHub', link: 'https://github.com/huzidabanzhang' }
+        ],
+        sidebar: [
+            '/first.md'
+        ]
+    }
+}
